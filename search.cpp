812e13751cab6cbd1299f3a6953f8de36ad23c12
Kayden Tarvaran <ktarvaran0@saddleback.edu>
2025-08-29 16:40:50 -0700

    finished search.cpp

--- search.cpp
+++ search.cpp
@@ -1,1 +1,38 @@
 
+/************************************************
+* Programmer: Kayden Tarvaran, Sean Hanabusa
+* Assignment: G1
+* Professor: Dr. Barnett
+* Purpose: Linear Search list of movies
+*************************************************/
+
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+
+int linearSearch(const vector<string>& list, const string& key){
+    string title = "";
+    string lowercaseKey = key;
+
+    for (char &c : lowercaseKey){
+            c = tolower(c);
+        }
+
+    for (int i = 0; i < list.size(); i++){
+        title = list[i];
+
+        for (char &c : title){
+            c = tolower(c);
+        }
+        if (title.find(lowercaseKey) != string::npos){
+
+            return i;
+        }
+    }
+    return -1;
+
+}
+
+

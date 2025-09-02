af810a10662f000055852a6a16fd36af4a3ac866
Kayden Tarvaran <ktarvaran0@saddleback.edu>
2025-09-02 13:39:25 -0700

    Updated search.cpp with comments

--- search.cpp
+++ search.cpp
@@ -14,14 +14,19 @@ using namespace std;
 #include <string>
 #include <cctype>
 
+//Linear search function
 int linearSearch(const vector<string>& list, const string& key){
+
+    //Defining temp variables
     string title = "";
     string lowercaseKey = key;
 
+    //Creating a lowercase version of the key
     for (char &c : lowercaseKey){
             c = tolower(c);
         }
 
+    //Loop to compare key to current value and determine if it is found or not
     for (int i = 0; i < list.size(); i++){
         title = list[i];
 
